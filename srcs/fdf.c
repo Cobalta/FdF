4801e7794470cb9bfcc66bd7433f101d567e3d17
Enzo Bourgeois <ebourgeo@z3r8p2.le-101.fr>
2019-06-27 20:04:28 +0200

    fdf rdm

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -28,10 +28,26 @@ int		deal_key(int key, t_win *win)
 
 int		ft_fdf(t_win *win)
 {
+	t_coor coor;
+
+	coor.x1 = 0;
+	coor.x2 = 2560;
+	coor.y1 = 0;
+	coor.y2 = 1315;
+
+	t_coor coor2;
+
+	coor2.x1 = 2560;
+	coor2.x2 = 0;
+	coor2.y1 = 0;
+	coor2.y2 = 1315;
+
 	win->x = 2560;
 	win->y = 1315;
 	win->mlx_ptr = mlx_init();
 	win->win_ptr = mlx_new_window(win->mlx_ptr, win->x, win->y, "FdF");
+	line_tracer(&coor, win);
+	line_tracer(&coor2, win);
 	mlx_key_hook(win->win_ptr, deal_key, win);
 	mlx_loop(win->mlx_ptr);
 	return (0);
